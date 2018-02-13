c2a6e2771911b64535313db677940b669378487b
Alexander Shtepa <rocko61rus@gmail.com>
2018-02-14 00:32:11 +0300

    Шаблон сайта

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -2,6 +2,112 @@
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 }
+$curPage = $APPLICATION->GetCurPage();
 ?>
+<? if ($curPage != '/'): ?>
+    </div>
+<? endif ?>
+<footer class="wow bounceInUp" style="visibility: visible; animation-name: bounceInUp;">
+    <div class="container">
+        <div class="row">
+            <div class="footer_position_1 col-md-3">
+                <div class="logo"></div>
+                <p><span style="font-size: 14pt;">Продажа автозапчастей</span></p>
+                <p><span style="font-size: 14pt;">Телефон для заказа:</span></p>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td><img class="alignnone size-full wp-image-129" src="<?= SITE_TEMPLATE_PATH ?>/images/telephone.png"
+                                 alt="" width="14" height="19"></td>
+                        <td><span style="font-size: 18pt;"><strong>+79081993021</strong></span></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <style>.footer_position_1 table img {
+                        margin-right: 10px;
+                    }</style>
+            </div>
+            <div class="footer_position_2 col-md-6">
+                <p><strong><span style="font-size: 18pt; color: #57a453;">Обратная связь:</span></strong></p>
+                <div role="form" class="wpcf7" id="wpcf7-f130-o2" lang="ru-RU" dir="ltr">
+                    <div class="screen-reader-response"></div>
+                    <form action="http://demo.click5.ru/dogovor/546228-39/index.html#wpcf7-f130-o2" method="post"
+                          class="wpcf7-form" novalidate="novalidate">
+                        <div style="display: none;">
+                            <input type="hidden" name="_wpcf7" value="130">
+                            <input type="hidden" name="_wpcf7_version" value="4.8">
+                            <input type="hidden" name="_wpcf7_locale" value="ru_RU">
+                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f130-o2">
+                            <input type="hidden" name="_wpcf7_container_post" value="0">
+                            <input type="hidden" name="_wpcf7_nonce" value="a3c9c6d1b6">
+                        </div>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td><span class="wpcf7-form-control-wrap text-1"><input type="text" name="text-1"
+                                                                                        value="" size="40"
+                                                                                        class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                                                                        aria-required="true"
+                                                                                        aria-invalid="false"
+                                                                                        placeholder="Ваше имя..."></span>
+                                </td>
+                                <td rowspan="3"><span class="wpcf7-form-control-wrap textarea-1"><textarea
+                                                name="textarea-1" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea"
+                                                aria-invalid="false" placeholder="Текст сообщения..."></textarea></span></td>
+                            </tr>
+                            <tr>
+                                <td><span class="wpcf7-form-control-wrap text-2"><input type="text" name="text-2"
+                                                                                        value="" size="40"
+                                                                                        class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                                                                        aria-required="true"
+                                                                                        aria-invalid="false"
+                                                                                        placeholder="Номер телефона..."></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="submit" value="Отправить" class="wpcf7-form-control wpcf7-submit"><span
+                                            class="ajax-loader"></span></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div class="wpcf7-response-output wpcf7-display-none"></div>
+                    </form>
+                </div>
+                <style>.footer_position_2 table td {
+                        padding: 0;
+                        vertical-align: top;
+                    }
+
+                    .footer_position_2 table {
+                        height: 160px;
+                    }</style>
+            </div>
+            <div class="footer_position_3 col-md-3">
+                <p><strong><span style="font-size: 18pt; color: #57a453;">Разделы:</span></strong></p>
+                <div class="menu-verhnee-menyu-container">
+                    <ul id="menu-verhnee-menyu-1" class="menu nav navbar-nav">
+                        <?
+                        $APPLICATION->IncludeComponent('bitrix:menu', 'bottom', array(
+                            'ALLOW_MULTI_SELECT' => 'N',    // Allow several menu items to be highlighted as active
+                            'CHILD_MENU_TYPE' => 'left',    // Menu type for child levels
+                            'COMPONENT_TEMPLATE' => '.default',
+                            'DELAY' => 'N',    // Delay building of menu template
+                            'MAX_LEVEL' => '2',    // Menu depth level
+                            'MENU_CACHE_GET_VARS' => '',    // Important query variables
+                            'MENU_CACHE_TIME' => '3600',    // Cache time (sec.)
+                            'MENU_CACHE_TYPE' => 'A',    // Cache type
+                            'MENU_CACHE_USE_GROUPS' => 'N',    // Respect Access Permissions
+                            'ROOT_MENU_TYPE' => 'bottom',    // Menu type for root level
+                            'USE_EXT' => 'N',    // Use files .menu-type.menu_ext.php for menus
+                        ),
+                            false
+                        ); ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,15 +1,134 @@
-<?php
+<?
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Page\Asset;
+use Quetzal\Environment\EnvironmentManager;
+
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
-	die();
+    die();
 }
 
-IncludeTemplateLangFile(__FILE__);
+Loc::loadMessages(__FILE__);
+
+$asset = Asset::getInstance();
+$env = EnvironmentManager::getInstance();
+
+$curPage = $APPLICATION->GetCurPage();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<?$APPLICATION->ShowHead()?>
-	<title><?$APPLICATION->ShowTitle()?></title>
+    <meta charset="UTF-8"/>
+    <title><? $APPLICATION->ShowTitle() ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui"/>
+    <meta name="format-detection" content="telephone=no"/>
+    <?
+    $APPLICATION->SetPageProperty('og:url', 'http://' . SITE_SERVER_NAME . '/');
+    //$APPLICATION->SetPageProperty('og:image', 'http://' . SITE_SERVER_NAME . SITE_TEMPLATE_PATH . '/images/logo.png');
+    $APPLICATION->ShowMeta('robots');
+    $APPLICATION->ShowMeta('keywords');
+    $APPLICATION->ShowMeta('description');
+
+    $APPLICATION->ShowMeta('og:url');
+    $APPLICATION->ShowMeta('og:title');
+    $APPLICATION->ShowMeta('og:image');
+    $APPLICATION->ShowMeta('og:description');
+    ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i,900&subset=cyrillic" rel="stylesheet">
+    <?
+    $asset->addCss(sprintf('%s/css/animate.css', SITE_TEMPLATE_PATH));
+    $asset->addCss(sprintf('%s/css/bootstrap.min.css', SITE_TEMPLATE_PATH));
+    $asset->addCss(sprintf('%s/css/jquery.fancybox-1.3.8.min.css', SITE_TEMPLATE_PATH));
+    $asset->addCss(sprintf('%s/css/style.css', SITE_TEMPLATE_PATH));
+
+    $asset->addJs(sprintf('%s/js/jquery.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/bootstrap.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/jquery.bxslider.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/jquery.easing.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/jquery.fancybox-1.3.8.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/jquery.mousewheel.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/jquery-migrate.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/wow.min.js', SITE_TEMPLATE_PATH));
+    $asset->addJs(sprintf('%s/js/main.js', SITE_TEMPLATE_PATH));
+    ?>
+    <?
+    $APPLICATION->ShowCSS();
+    $APPLICATION->ShowHeadStrings();
+    $APPLICATION->ShowHeadScripts();
+    ?>
+
+    <script>new WOW().init();</script>
 </head>
 <body>
-	<?$APPLICATION->ShowPanel()?>
+<?$APPLICATION->ShowPanel()?>
+<div class="header_bg <? if ($curPage != '/'): ?>__inner<? endif ?>" style="background-image: url(<?= SITE_TEMPLATE_PATH ?>/images/bg.jpg);">
+    <div class="container header wow bounceInDown" style="visibility: visible; animation-name: bounceInDown;">
+        <div class="row">
+            <div class="header_position_1 col-md-2">
+                <a class="logo" href="/"></a>
+            </div>
+            <div class="header_position_4 col-md-2">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <img class="alignnone size-full wp-image-115" src="<?= SITE_TEMPLATE_PATH ?>/images/email-1.png" alt="" width="15" height="10"></td>
+                        <td>
+                            <? $APPLICATION->IncludeFile('/local/include/main/header_email.php'); ?>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <nav class="navbar navbar-default bounceInDown wow col-md-8"
+                 style="visibility: visible; animation-name: bounceInDown;">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="header_nav navbar-collapse collapse" id="bs-example-navbar-collapse-1">
+                    <div class="menu-header">
+                        <ul id="menu-verhnee-menyu" class="menu nav navbar-nav">
+                            <?
+                            $APPLICATION->IncludeComponent('bitrix:menu', 'top', array(
+                                'ALLOW_MULTI_SELECT' => 'N',    // Allow several menu items to be highlighted as active
+                                'CHILD_MENU_TYPE' => 'left',    // Menu type for child levels
+                                'COMPONENT_TEMPLATE' => '.default',
+                                'DELAY' => 'N',    // Delay building of menu template
+                                'MAX_LEVEL' => '2',    // Menu depth level
+                                'MENU_CACHE_GET_VARS' => '',    // Important query variables
+                                'MENU_CACHE_TIME' => '3600',    // Cache time (sec.)
+                                'MENU_CACHE_TYPE' => 'A',    // Cache type
+                                'MENU_CACHE_USE_GROUPS' => 'N',    // Respect Access Permissions
+                                'ROOT_MENU_TYPE' => 'top',    // Menu type for root level
+                                'USE_EXT' => 'N',    // Use files .menu-type.menu_ext.php for menus
+                            ),
+                                false
+                            ); ?>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="clear"></div>
+            <? if ($curPage == '/'): ?>
+                <div class="header_slide_text">
+                    <? $APPLICATION->IncludeFile('/local/include/main/header_title.php'); ?>
+                </div>
+                <? $APPLICATION->IncludeFile('/local/include/main/header_form.php'); ?>
+            <? endif ?>
+        </div>
+    </div>
+</div>
+<div class="clear"></div>
+<? if ($curPage != '/'): ?>
+    <div class="inner-page">
+<? endif ?>
