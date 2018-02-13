94b645b1632e5b6ce3db9100812f6fae4f8a69b6
Alexander Shtepa <rocko61rus@gmail.com>
2018-02-14 00:30:55 +0300

    Включаемые области

---
+++ local/include/main/header_email.php
@@ -0,0 +1,1 @@
+<a href="mailto:leo469@yandex.ru">leo469@yandex.ru</a>
---
+++ local/include/main/header_form.php
@@ -0,0 +1,39 @@
+<div class="header_slide_form">
+    <div class="header_slide_form_title">Отправьте заявку</div>
+    <div role="form" class="wpcf7" id="wpcf7-f4-o1" lang="ru-RU" dir="ltr">
+        <div class="screen-reader-response"></div>
+        <form action="http://demo.click5.ru/dogovor/546228-39/index.html#wpcf7-f4-o1" method="post"
+              class="wpcf7-form" novalidate="novalidate">
+            <div style="display: none;">
+                <input type="hidden" name="_wpcf7" value="4">
+                <input type="hidden" name="_wpcf7_version" value="4.8">
+                <input type="hidden" name="_wpcf7_locale" value="ru_RU">
+                <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f4-o1">
+                <input type="hidden" name="_wpcf7_container_post" value="0">
+                <input type="hidden" name="_wpcf7_nonce" value="cb927aecf8">
+            </div>
+            <div class="input-group"><span class="wpcf7-form-control-wrap txt_1">
+                                    <input type="text"
+                                           name="txt_1"
+                                           value="" size="40"
+                                           class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control"
+                                           aria-required="true"
+                                           aria-invalid="false"
+                                           placeholder="Имя"></span>
+            </div>
+            <div class="input-group"><span class="wpcf7-form-control-wrap txt_2">
+                                    <input type="text"
+                                           name="txt_2"
+                                           value="" size="40"
+                                           class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control"
+                                           aria-required="true"
+                                           aria-invalid="false"
+                                           placeholder="Телефон"></span>
+            </div>
+            <div class="input-group"><input type="submit" value="Отправить"
+                                            class="wpcf7-form-control wpcf7-submit btn btn-default"><span
+                    class="ajax-loader"></span></div>
+            <div class="wpcf7-response-output wpcf7-display-none"></div>
+        </form>
+    </div>
+</div>
