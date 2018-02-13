d999ffb391e0c6eb07a36111bc629a70fcb6ea62
Alexander Shtepa <rocko61rus@gmail.com>
2018-02-14 00:30:41 +0300

    Верхнее и нижнее меню

---
+++ .bottom.menu.php
@@ -0,0 +1,38 @@
+<?
+$aMenuLinks = array(
+    array(
+        'Главная',
+        '/',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Каталог',
+        '/catalog/',
+        array(),
+        array('class' => 'menu-item-has-children dropdown'),
+        ''
+    ),
+    array(
+        'О компании',
+        '/#company',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Отзывы',
+        '/#reviews',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Оплата и доставка',
+        '/delivery/',
+        array(),
+        array(),
+        ''
+    ),
+);
---
+++ .top.menu.php
@@ -0,0 +1,45 @@
+<?
+$aMenuLinks = array(
+    array(
+        'Главная',
+        '/',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Каталог',
+        '/catalog/',
+        array(),
+        array('class' => 'menu-item-has-children dropdown'),
+        ''
+    ),
+    array(
+        'Оплата и доставка',
+        '/delivery/',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'О компании',
+        '/#company',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Отзывы',
+        '/#reviews',
+        array(),
+        array(),
+        ''
+    ),
+    array(
+        'Контакты',
+        '/#contacts',
+        array(),
+        array(),
+        ''
+    )
+);
