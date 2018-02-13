b1437d0241ccfc23481360edc02369a23af93e3a
Alexander Shtepa <rocko61rus@gmail.com>
2018-02-14 00:31:59 +0300

    Страница доставки

---
+++ delivery/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+$APPLICATION->SetTitle('Оплата и доставка');
+?>
+
+<div class="inner-title">Оплата и доставка</div>
+<div class="inner-text">
+    Информация о доставке и оплате....
+</div>
+
+<?
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
