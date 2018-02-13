c21cd5a5cc5394993041d773b559346782eb4af3
Alexander Shtepa <rocko61rus@gmail.com>
2018-02-14 00:31:48 +0300

    Компоненты верхнего и нижнего меню

---
+++ local/templates/.default/components/bitrix/menu/bottom/template.php
@@ -0,0 +1,35 @@
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+foreach ($arResult as $arItem) {
+    if ($arParams['MAX_LEVEL'] == 1 && $arItem['DEPTH_LEVEL'] > 1) {
+        continue;
+    }
+
+    $customClasses = '';
+
+    echo sprintf(
+        '<li class="menu-item menu-item-type-custom menu-item-object-custom %s"><a href="%s">%s</a></li>',
+        $customClasses,
+        $arItem['LINK'],
+        $arItem['TEXT']
+    );
+}
+
+?>
+<?/*
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-131 dropdown">
+                            <a title="Наши услуги" target="_blank" data-toggle="dropdown" class="dropdown-toggle">Наши
+                                услуги <span class="caret"></span></a>
+                            <ul role="menu" class=" dropdown-menu">
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-132"><a
+                                            title="Пункт меню" href="http://click5.ru/nopage/" target="_blank">Пункт
+                                        меню</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-133"><a
+                                            title="Пункт меню" href="http://click5.ru/nopage/" target="_blank">Пункт
+                                        меню</a></li>
+                            </ul>
+                        </li>
+*/?>
---
+++ local/templates/.default/components/bitrix/menu/top/template.php
@@ -0,0 +1,37 @@
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+foreach ($arResult as $arItem) {
+    if ($arParams['MAX_LEVEL'] == 1 && $arItem['DEPTH_LEVEL'] > 1) {
+        continue;
+    }
+
+    $customClasses = '';
+
+    echo sprintf(
+        '<li class="menu-item menu-item-type-custom menu-item-object-custom %s"><a href="%s">%s</a></li>',
+        $customClasses,
+        $arItem['LINK'],
+        $arItem['TEXT']
+    );
+}
+
+?>
+<?/*
+<li id="menu-item-131"
+    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-131 dropdown">
+    <a title="Наши услуги" target="_blank" data-toggle="dropdown" class="dropdown-toggle">Каталог<span class="caret"></span></a>
+    <ul role="menu" class=" dropdown-menu">
+        <li id="menu-item-132"
+            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-132"><a
+                title="Пункт меню" href="http://click5.ru/nopage/" target="_blank">Пункт
+                меню</a></li>
+        <li id="menu-item-133"
+            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-133"><a
+                title="Пункт меню" href="http://click5.ru/nopage/" target="_blank">Пункт
+                меню</a></li>
+    </ul>
+</li>
+*/?>
